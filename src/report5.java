4809199856ab2619209ffab49fc53c0adc0dc2df
kinjoukouhei <e235725@ie.u-ryukyu.ac.jp>
2023-12-11 12:53:49 +0900

    1st

--- src/report5.java
+++ src/report5.java
@@ -1,6 +1,11 @@
-class report5{
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+class report5 {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException a) {
+            System.out.println("NullPointerException occurred.");
+            System.out.println(a.getMessage());
+        }
     }
 }
